a9acb8ab58e12ed6dfffd928d6fc6c83f1f7bbfa
mohammad iko <developer.iko.mike@gmail.com>
2025-07-19 10:06:16 -0700

    initial commit

---
+++ src/Components/App.jsx
@@ -0,0 +1,7 @@
+import E1 from "./Episode1/E1";
+
+const App = () => {
+  return <E1/>;
+};
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css' 
+import App from './Components/App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+// imarena.ai
+// diract chat => select chat => copy code
