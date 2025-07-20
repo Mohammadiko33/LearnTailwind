456acdbe322bdd42835823be9ac64e8c84ecee6d
mohammad iko <developer.iko.mike@gmail.com>
2025-07-20 11:15:17 -0700

    add episode 10

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,6 @@ export default {
       center: true,
       padding: '.5rem',
     },
-    screens: {
-      "xl": {"max": "1280px"},
-      "lg": {"max": "992px"},
-      "md": {"max": "768px"},
-      "sm": {"max": "576px"},
-    },
     extend: {},
   },
   plugins: [],
