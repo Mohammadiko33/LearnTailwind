7eb258a99148e0bf2fa0fde227be8f0b01109b66
mohammad iko <developer.iko.mike@gmail.com>
2025-07-20 14:37:14 -0700

    add episode 13

---
+++ src/Episodes/E13.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const E13 = () => {
+  return (
+    <div className="container mx-auto mt-6">
+      <div className="container mx-auto mt-6">
+        <div className="grid grid-cols-4 grid-rows-2 gap-5">
+          <div className="h-56 bg-rose-300 col-span-2" />
+          <div className="h-56 bg-rose-400" />
+          <div className="bg-rose-500 row-span-2" />
+          <div className="h-56 bg-rose-600 col-span-3" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default E13;
