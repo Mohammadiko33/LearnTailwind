240751d73a4fa951737ad44055aef3cc599a8188
mohammad iko <developer.iko.mike@gmail.com>
2025-07-20 16:43:05 -0700

    add episode 14

---
+++ src/Episodes/E14.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const E13 = () => {
+  return (
+    <>
+      <div className="container space-y-5 bg-slate-100">
+        <div className="h-48 bg-yellow-100" />
+        <div className="h-48 bg-yellow-300" />
+        <div className="h-48 bg-yellow-500" />
+        <div className="h-48 bg-yellow-700" />
+        <div className="h-48 bg-yellow-900" />
+      </div>
+    </>
+  );
+};
+
+export default E13;
