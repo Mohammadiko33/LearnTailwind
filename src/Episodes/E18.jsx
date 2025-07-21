e053dcff1ab800a41a4408ff0997e6937fbe4e79
mohammad iko <developer.iko.mike@gmail.com>
2025-07-20 17:34:32 -0700

    add episode 18

---
+++ src/Episodes/E18.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const E18 = () => {
+  return (
+    <div className='fullCenter'>
+        <div 
+        className="h-64 w-64 bg-rose-500 rounded-md delay-75 duration-150 ease-linear cursor-none hover:bg-rose-400"
+        />
+        <div className='h-64 w-64 bg-rose-500 animate-spin'/>
+        <div className='h-64 w-64 bg-rose-500 animate-ping'/>
+        <div className='h-64 w-64 bg-rose-500 animate-pulse'/>
+        <div className='h-64 w-64 bg-rose-500 animate-bounce'/>
+    </div>
+  )
+}
+
+export default E18
