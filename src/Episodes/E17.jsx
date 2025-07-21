7a020b4c7030f95c0e8c4465e1b6d26f319cc631
mohammad iko <developer.iko.mike@gmail.com>
2025-07-20 17:25:03 -0700

    add episode 17

---
+++ src/Episodes/E17.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const E17 = () => {
+  return (
+    <div className=" max-w-sm mx-auto pt3 divide-y-4 border-sky-300 rounded-bl-lg overflow-hidden">
+      <div className="h-20 bg-rose-500 border-4 border-black" />
+      <div className="h-20 bg-rose-500 border-4 border-black" />
+      <div className="h-20 bg-rose-500 border-4 border-black" />
+      <div className="h-20 bg-rose-500 border-4 border-black" />
+      <div className="h-20 bg-rose-500 border-4 border-black" />
+    </div>
+  );
+};
+
+export default E17;
