2f49673f802602910dacb069730abae3aeccc53e
mohammad iko <developer.iko.mike@gmail.com>
2025-07-20 17:04:32 -0700

    add episode 16

---
+++ src/Episodes/E16.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const E16 = () => {
+  return (
+    <div className='fullCenterCol'>
+        <h1 className='font-irsansx'>محمد یکو</h1>
+        <p className='font-iryekanx'>  طراح و توسعه دهنه وب</p>
+    </div>
+  )
+}
+
+export default E16;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ export default {
       center: true,
       padding: '.5rem',
     },
+    fontFamily: {
+      "irsansx": "IRANSansX",
+      "iryekanx": "IRANYekanX"
+      // اسم کی که مینویسیم اسمی هست که میخایم توی پروژه ازش استفاده کنیم
+    // اسم ولیو که مینویسیم باید دقیقا اسمی باشه که توی اپ سی اس اس توی فونت فیس اسم رو توی فونت فمیلی گذاشتیم
+    },
     extend: {},
   },
   plugins: [],
